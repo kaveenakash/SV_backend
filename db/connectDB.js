3f4d6635cb7354c1293893ec28e62b6fd6d36f20
kaveenakash <kaveenakashya@gmail.com>
2021-05-28 19:34:17 +0530

    fixed mongodb connection

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE,{
-    useNewUrlParser:true,
-    useFindAndModify:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true
-})
-.then(() => console.log(`DB Connected Estabilished`))
-.catch((err) => console.log('DB connection  error',err))
+// mongoose.connect(process.env.DATABASE,{
+//     useNewUrlParser:true,
+//     useFindAndModify:true,
+//     useUnifiedTopology:true,
+//     useCreateIndex:true
+// })
+// .then(() => console.log(`DB Connected Estabilished`))
+// .catch((err) => console.log('DB connection  error',err))
+
+const dbURI = 'mongodb+srv://SOCIAL_admin:SOCIAL_123@cluster0.kibum.mongodb.net/Social?retryWrites=true&w=majority'
+mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
+.then((result) => console.log('connected to db'))
